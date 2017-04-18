8740a7c282f30e19ced91e559300a5e41071a9f4
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2017-04-18 16:17:11 -0400

    distribution setup and configuration files

--- Makefile
+++ Makefile
@@ -1,9 +1,28 @@
 # See file COPYING distributed with unf for copyright and license.
 
+default : build
+
 test : 
 	python -m unittest -vb tests
 
+build : dist/unf-0.1.0.tar.gz
+
+dist/unf-0.1.0.tar.gz : 
+	python setup.py sdist
+
+register : 
+	python setup.py register
+
+upload : 
+	python setup.py sdist upload
+
+check : 
+	python setup.py check
+
 clean : 
-	rm *.pyc
+	rm -f MANIFEST *.pyc
+
+clobber : clean
+	rm -rf dist
 
 # eof
---
+++ setup.py
@@ -0,0 +1,25 @@
+# See file COPYING distributed with unf for copyright and license.
+
+from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(name='unf', 
+      version='0.1.0', 
+      description='Universal Numeric Fingerprints', 
+      author='Christian Haselgrove', 
+      author_email='christian.haselgrove@umassmed.edu', 
+      url='https://github.com/chaselgrove/python-unf', 
+      py_modules=['unf'], 
+      classifiers=['Development Status :: 3 - Alpha', 
+                   'Environment :: Console', 
+                   'Intended Audience :: Science/Research', 
+                   'License :: OSI Approved :: BSD License', 
+                   'Operating System :: OS Independent', 
+                   'Programming Language :: Python', 
+                   'Topic :: Scientific/Engineering'], 
+      license='BSD license', 
+      long_description=long_description
+     )
+
+# eof
