5ce410201ff5780ecab42527ed14cf58b613b236
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2017-04-21 15:05:29 -0400

    version 0.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 long_description = open('README.rst').read()
 
 setup(name='unf', 
-      version='0.5.0', 
+      version='0.5.1', 
       description='Universal Numeric Fingerprints', 
       author='Christian Haselgrove', 
       author_email='christian.haselgrove@umassmed.edu', 
